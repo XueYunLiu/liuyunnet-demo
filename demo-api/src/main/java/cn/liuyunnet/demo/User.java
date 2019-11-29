870da200c5a46c05b2550ef6a278331e4e14e756
liuxueyun <604740360@qq.com>
2019-11-29 14:51:01 +0800

    init

---
+++ demo-api/src/main/java/cn/liuyunnet/demo/User.java
@@ -0,0 +1,22 @@
+package cn.liuyunnet.demo;
+
+import lombok.Data;
+
+/**
+ * Description
+ *
+ * @author xueyun.liu resico
+ * @since 2019/11/29
+ */
+@Data
+public class User {
+    /**
+     * 用户名
+     */
+    private String userName;
+    /**
+     * 密码
+     */
+    private String password;
+
+}
