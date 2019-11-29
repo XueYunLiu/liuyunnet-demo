b9a77057753f9555fbe0881ff33c18d6662d4a5d
liuxueyun <604740360@qq.com>
2019-11-29 14:56:36 +0800

    init

---
+++ demo-api/src/main/java/cn/liuyunnet/demo/LoginController.java
@@ -0,0 +1,29 @@
+package cn.liuyunnet.demo;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description
+ *
+ * @author xueyun.liu resico
+ * @since 2019/11/29
+ */
+@RestController
+@RequestMapping("/rest")
+public class LoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public Boolean login(@RequestBody User user) {
+        if ("admin".equals(user.getUserName())) {
+            if (!"111111".equals(user.getPassword())) {
+                return Boolean.FALSE;
+            }
+        }
+        System.out.println("用户名" + user.getUserName());
+        System.out.println("用户密码" + user.getPassword());
+        return Boolean.TRUE;
+    }
+}
