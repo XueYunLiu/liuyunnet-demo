0c6b3a3c5d726ae610d48617ae58b703c408644f
liuxueyun <604740360@qq.com>
2019-12-01 02:27:18 +0800

    test

--- demo-web/config/index.js
+++ demo-web/config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     // proxyTable: {},
     proxyTable: {
       '/rest': {
-        target: 'http://localhost:8081',//设置你调用的接口域名和端口号 别忘了加http
+        target: 'http://47.111.114.165:8081',//设置你调用的接口域名和端口号 别忘了加http
         changeOrigin: true,
         pathRewrite: {
           '^/rest': '/rest'
